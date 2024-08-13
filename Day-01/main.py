3cb4e4479bf3c5558d4dca6740e85aee1519806a
BeesuVarshini <beesuvarshini@gmail.com>
2024-08-13 19:42:56 +0530

    day 1 completed

---
+++ Day-01/main.py
@@ -0,0 +1,70 @@
+'''Open the python interactive shell and do the following operations. The operands are 3 and 4.
+    addition(+)
+    subtraction(-)
+    multiplication(*)
+    modulus(%)
+    division(/)
+    exponential(**)
+    floor division operator(//)'''
+
+a = int(input("enter a value :"))
+b = int(input("enter b value :"))
+print("addition = ",a+b)
+print("subtraction =",a-b)
+print("multiplication =",a*b)
+print("division =",a/b)
+print("exponential =",a**b)
+print("floor division =",a//b)
+
+'''Write strings on the python interactive shell. The strings are the following:
+    Your name
+    Your family name
+    Your country
+    I am enjoying 30 days of python'''
+
+print("Your Name : Varshini")
+print("Your family name : Beesu")
+print("Your country : India")
+print("I am enjoying 30 days of python")
+
+'''Check the data types of the following data:
+    10
+    9.8
+    3.14
+    4 - 4j
+    ['Asabeneh', 'Python', 'Finland']
+    Your name
+    Your family name
+    Your country'''
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type('Your name'))
+print(type('Your familyname'))
+print(type('Your country'))
+
+#Find an Euclidian distance between (2, 3) and (10, 8)
+ED = ((2-10)**2 + (3-8)**2)**0.5 
+print("Euclidean distance :",ED)
+
+#Write an example for different Python data types such as Number(Integer, Float, Complex), String, Boolean, List, Tuple, Set and Dictionary.
+print("Integer = 5")
+print("Float = 4.5")
+print("Complex = 3+5j")
+print("String = 'apple'")
+print("Boolean = True")
+print("List = ['orange','mango',5]")
+print("Tuple = ('carrot','milk',6)")
+print("Set = {4,7,9}")
+print("Dictionary = {'H':157,'colors':['black','white']}")
+
+
+
+
+
+
+
+
